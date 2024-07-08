ccc0df4442e72f0efb727bb69b0487cdc9d191c9
Muralikrishnakoduru462 <muralikrishnakoduru462@gmail>
2024-07-08 22:31:09 +0530

    namaste-practice

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
 const heading = React.createElement("h1", {}, "hii koduru muralikrishna");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
